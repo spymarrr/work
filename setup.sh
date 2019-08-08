d6b12f33b683686b92b6b630ad0571c2ccc2148b
spymarrr <50561801+spymarrr@users.noreply.github.com>
2019-08-08 18:45:52 +0100

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt -o Acquire::Check-Valid-Until=false update -y && sudo apt upgrade -y
-sudo apt install vim ccze ansible zip unzip -y
-unzip ansible.zip
-ansible-playbook -i ansible/hosts ansible/rs1.yml --user=hessaddo --extra-vars "ansible_sudo_pass=56452312"
