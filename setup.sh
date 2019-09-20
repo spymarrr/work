cbf96ec17f5d03123b9675b60a2b1902ca5f3e3f
spymarrr <spymarrr@gmail.com>
2019-09-20 02:56:20 +0000

    5.1

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 ip=''
 gateway=''
 
-#sudo apt -o Acquire::Check-Valid-Until=false update -y && sudo apt upgrade -y
-#sudo apt install vim ccze ansible zip unzip -y
-#unzip ansible.zip
+sudo apt -o Acquire::Check-Valid-Until=false update -y && sudo apt upgrade -y
+sudo apt install vim ccze ansible zip unzip -y
+unzip ansible.zip
 find ./ansible -type f -exec sed -i "s/{{ ip }}/$ip/g" {} +
 find ./ansible -type f -exec sed -i "s/{{ gateway }}/$gateway/g" {} +
-#ansible-playbook -i ansible/hosts ansible/rs1.yml --user=hessaddo --extra-vars "ansible_sudo_pass=56452312"
+ansible-playbook -i ansible/hosts ansible/rs1.yml --user=hessaddo --extra-vars "ansible_sudo_pass=56452312"
